2834aad1dddc337853d748773a799bc3083618f0
asiimwe7610 <asiimwe7610@gmail.com>
2023-07-28 09:19:00 +0300

    Building Screens and google aunthentication

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StackNavigator from './StackNavigator';
+import { AuthProvider } from './Hooks/useAuth';
+import { useContext } from 'react';
+// import t from 'tailwind-rn'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthProvider>
+      <StackNavigator/>
+      </AuthProvider>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
